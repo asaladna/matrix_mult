02635a9e1bdf17c9735607ff9a0238c1a5503d38
alex <alexsaladna@gmail.com>
2016-12-05 06:28:47 -0600

    output sort of working

--- pos_delay.v
+++ pos_delay.v
@@ -1,15 +1,50 @@
 
-module pos_delay( din, dout, clk);
-
-	input clk;
+module pos_delay( din, dout, clk, reset);
+/*
+	input clk, reset;
 	input  din;
 	output  dout;
-
+	reg pstate;
 	reg dout;
 
-    always @(posedge clk)
+
+		
+	always @ (posedge clk)
 		begin
-			dout = din;
+			if (reset == 1'b1) dout = din;
+
+			else if (reset == 1'b0)  dout = din;
+		end
+
+	
+endmodule
+*/
+
+	input clk, reset;
+	input  din;
+	output  dout;
+	reg pstate;
+	reg dout;
+
+
+	parameter S0=1'b0, S1=1'b1;
+		
+	always @ (posedge clk)
+		begin
+			if (reset == 1'b1) pstate = S0;
+
+			else if (reset == 1'b0)  pstate = S1;
 		end
 		
+	always @ (pstate)
+		begin
+			
+			case(pstate)		
+				S0: dout = din;
+				S1: dout = din;
+			endcase
+		end
+		
+		
+
 endmodule
